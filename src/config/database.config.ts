2253460315fc4bd709eb33c25224f0725c93c51e
kfoq7 <frbrizoi@gmail.com>
2024-11-21 10:04:52 -0500

    chore: added username to local database connection

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -14,6 +14,7 @@ export const typeOrmConfig: () => TypeOrmModuleOptions = () => ({
   type: 'mysql',
   host: process.env.MYSQL_HOST,
   port: Number(process.env.MYSQL_PORT) || 3306,
+  username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   entities: [pathEntities],
